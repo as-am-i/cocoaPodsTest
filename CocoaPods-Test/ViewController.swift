191abebfbd5107cad2bd4a24e8723c11742a2f90
Asami Tanii <g033.9521@gmail.com>
2018-07-18 15:19:39 -0700

    Initial Commit

---
+++ CocoaPods-Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CocoaPods-Test
+//
+//  Created by 谷井朝美 on 2018-07-18.
+//  Copyright © 2018 Asami Tanii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
